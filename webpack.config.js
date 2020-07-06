6b8e04f1c8085a5c53ab737109b161b195f34e27
Brij Kathiriya <brij.kathiria@gmail.com>
2020-07-06 16:15:45 -0400

    completed header

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const postCSSPlugins  = [
     require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
+    require('postcss-hexrgba'),
     require('autoprefixer')
-
 ]
 
 module.exports = {
