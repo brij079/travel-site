99f398b73ff378052892ecc331320c3a6d3bd27d
Brij Kathiriya <brij.kathiria@gmail.com>
2020-07-07 11:35:17 -0400

    testing cloud function

--- cloud-function/secret-area.js
+++ cloud-function/secret-area.js
@@ -1,6 +1,28 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to Secret Area"
-    })
+    const secretContent = `
+    <h3> Welcome to Secret area</h3>
+    <p>Sky is <strong>blue</strong><p>`
+    
+    let body
+
+
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else{
+        body = {}
+    }
+
+    if (body.passwod == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+    } else {
+        callback(null, {
+            statusCode: 401
+            
+        })
+    }
+    
+    
 }
