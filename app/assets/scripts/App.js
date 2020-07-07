2dc27b67bdbe9cb1f5c5494533aaa19386838681
Brij Kathiriya <brij.kathiria@gmail.com>
2020-07-06 20:21:09 -0400

    completed modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,12 +3,25 @@ import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickeyHeader from './modules/StickyHeader'
 
-let stickeyHeader =new StickeyHeader;
+new StickeyHeader;
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+new MobileMenu();
+let modal
 
-
-const mobileMenu = new MobileMenu();
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault()
+        if(typeof modal == "undefined"){
+            import('./modules/Modal').then((x)=>{
+                modal = new x.default()
+                setTimeout(()=>{modal.openTheModal()}, 20)
+            }).catch(()=>{console.log('error')})
+        }else{
+            modal.openTheModal()
+        }
+    })
+})
 
 if(module.hot){
     module.hot.accept()
